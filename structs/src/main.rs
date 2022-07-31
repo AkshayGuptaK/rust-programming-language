d7902431c8514311029c4c3ae24dc78578bb71ea
Akshay Gupta <akshaygrkesh@gmail.com>
2022-07-31 13:49:59 +0530

    Structs: Rectangle comparison

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,12 +5,23 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
     fn area(&self) -> u32 {
         self.width * self.height
     }
 
     fn width(&self) -> bool {
         self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
@@ -20,9 +31,21 @@ fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 20,
+        height: 40,
+    };
+
     println!("The rectangle is {:#?}", rect1);
 
     println!("The rectangle has width: {}", rect1.width());
 
     println!("The area of the rectangle is {}.", rect1.area());
+
+    println!("This is another rectangle {:#?}", rect2);
+
+    println!(
+        "Can the first rectangle hold the second? {}",
+        rect1.can_hold(&rect2)
+    );
 }
